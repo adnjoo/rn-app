86c07238914ae4aac1bdbac079f2cd83ed38f9e5
Andrew Njoo <adnjoo@gmail.com>
2023-12-27 15:49:05 -0800

    Add axios and fetch data from PokeAPI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,28 @@
-import { StyleSheet } from 'react-native';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Image } from "react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
 
 export default function TabOneScreen() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/pikachu").then((res) => {
+      console.log(res.data);
+      setData(res.data);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View className="flex flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold capitalize">{data?.name}</Text>
+      <View className="my-8 h-px w-80 bg-gray-300 dark:bg-gray-700" />
+      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
+      <Image
+        source={{ uri: data?.sprites?.front_default }}
+        className="h-64 w-64"
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
