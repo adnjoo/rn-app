6c5a922862ae431ec79cee1272dfb2ebb5dd8280
Andrew Njoo <adnjoo@gmail.com>
2023-12-27 16:42:02 -0800

    fetch

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rn-app
+
+- react-native
+- nativewind
+- axios
+
+<img width='300' src='./assets/images/scrn.png'>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,63 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { Image, TextInput, Button, View, ScrollView, StyleSheet } from "react-native";
 import axios from "axios";
-import { Image } from "react-native";
 
-import EditScreenInfo from "../../components/EditScreenInfo";
-import { Text, View } from "../../components/Themed";
+import { Text } from "../../components/Themed";
 
 export default function TabOneScreen() {
+  const [pokemonName, setPokemonName] = useState("pikachu"); // Default Pokémon name
   const [data, setData] = useState({});
-  useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon/pikachu").then((res) => {
-      console.log(res.data);
-      setData(res.data);
-    });
-  }, []);
+
+  const fetchPokemonData = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching Pokémon data:", error);
+        setData({}); // Reset data on error
+      });
+  };
 
   return (
-    <View className="flex flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold capitalize">{data?.name}</Text>
-      <View className="my-8 h-px w-80 bg-gray-300 dark:bg-gray-700" />
-      {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
-      <Image
-        source={{ uri: data?.sprites?.front_default }}
-        className="h-64 w-64"
+    <ScrollView contentContainerStyle={styles.scrollView} >
+      {/* className="flex flex-1 items-center justify-center bg-white" */}
+      <Text className="my-4 text-2xl font-bold text-black">
+        Search for a Pokémon
+      </Text>
+      <TextInput
+        className="w-1/2 border p-2"
+        value={pokemonName}
+        onChangeText={setPokemonName}
+        onSubmitEditing={fetchPokemonData}
       />
-    </View>
+      <Button title="Search" onPress={fetchPokemonData} />
+      <View className="flex flex-1 items-center justify-center rounded-md border">
+        <Text className="text-xl font-bold capitalize text-black">
+          Name: {data?.name}
+        </Text>
+        <Text className="text-xl font-bold capitalize text-black">
+          Order: {data?.order}
+        </Text>
+        <Text className="text-xl font-bold capitalize text-black">
+          Types: {data?.types?.[0]?.type?.name}
+        </Text>
+        <Image
+          source={{ uri: data?.sprites?.front_default }}
+          className="h-64 w-64"
+        />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: "#fff",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "100%",
+  }
+})
